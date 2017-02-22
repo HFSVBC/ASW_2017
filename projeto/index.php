ba3149915c4bcb778a66ea54b8df26999dabd938
Hugo Curado <hugofsvbc@alunos.fc.ul.pt>
2017-02-22 10:29:00 +0000

    some changes -> 'projeto', 'projeto_mean'

--- projeto/index.php
+++ projeto/index.php
@@ -1,10 +1,10 @@
-<?php 
-	require_once("app/config.php");
+ <?php
+ 	require_once("app/config.php");
     require_once('../fykXspR43K/loginSession.php');
-	session_regenerate_id(true);
-	include "templates/header.php"; 
+ 	session_regenerate_id(true);
+ 	include "templates/header.php"; 
 ?>
 
 <?php
-	include "templates/footer.php"; 
+ 	include "templates/footer.php"; 
 ?>
