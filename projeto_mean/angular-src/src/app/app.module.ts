1e8f2cc719514cf6d441d4a5d42a59ec1e41f26f
Hugo Curado <hugofsvbc@alunos.fc.ul.pt>
2017-03-03 07:42:11 +0000

    yesterday changes -> 'mean'

--- projeto_mean/angular-src/src/app/app.module.ts
+++ projeto_mean/angular-src/src/app/app.module.ts
@@ -24,8 +24,10 @@ const appRoutes: Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
+  {path:'admin', component: AdminComponent, canActivate:[AuthGuard]},
   {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
   {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+  {path:'game', component: GameComponent, canActivate:[AuthGuard]},
 ];
 
 @NgModule({
--- projeto_mean/angular-src/src/app/components/admin/admin.component.ts
+++ projeto_mean/angular-src/src/app/components/admin/admin.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+	users: Object;
+	games: Object;
+
   constructor() { }
 
   ngOnInit() {
