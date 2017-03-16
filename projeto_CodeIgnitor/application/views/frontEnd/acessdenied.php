bfe48b00fac5adb651791bcccfdfb84f2230cfb1
pedroduarten9 <pedroduarteneto9@gmail.com>
2017-03-16 13:45:15 +0000

    search

---
+++ projeto_CodeIgnitor/application/views/frontEnd/acessdenied.php
@@ -0,0 +1,13 @@
+<article>
+  <div class='row'>
+    <div class='col-md-6 col-xs-12'>
+      <h2>Acesso negado</h2>
+      <div class='alert alert-danger'>
+        Utilizador sem permissões ou não autentificado.
+      </div>
+    </div>
+    <div class='col-md-6 col-xs-12'>
+      <img src='custom/images/acessDenied.jpg'>
+    </div>
+  </div>
+</article>
